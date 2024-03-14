aa72dbd9d0e768765806e64a4f1056b586921689
gstv <gustavodiefenbach@gmail.com>
2024-03-14 14:04:06 -0300

    s

---
+++ tokenStudioTest/tokenStudioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  tokenStudioTestApp.swift
+//  tokenStudioTest
+//
+//  Created by Gustavo Diefenbach on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct tokenStudioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
